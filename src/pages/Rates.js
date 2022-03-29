0eba0508d60e320524263a0c16cc74c311e97559
Sergey Antonov <flex62ryz@ya.ru>
2022-03-29 18:49:37 +0000

    Rates page rateline component

--- src/pages/Rates.js
+++ src/pages/Rates.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 
 const getRates = () => {
-    const response = {"status":"200","message":"rates","data":{"EURRUB":"71.3846","USDRUB":"58.059"}}
+    const response = { "status": "200", "message": "rates", "data": { "EURRUB": "71.3846", "USDRUB": "58.059" } }
     return response.data;
+}
+
+function RateLine({title, value}) {
+    return <div>
+        {title} - {value}
+    </div>
 }
 
 function Rates() {
     const [rates, setRates] = useState({});
-    
+
     useEffect(() => {
         const rates = getRates();
         setRates(rates);
@@ -16,9 +22,7 @@ function Rates() {
     return <>
         <h1>Rates</h1>
         <div>
-            {
-                Object.entries(rates).map(([key, value]) => <div>{key} - {value}</div>)
-            }
+            { Object.entries(rates).map(([title, value]) => <RateLine title={title} value={value} />) }
         </div>
     </>
 }
